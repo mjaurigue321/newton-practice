40b84cd0c095a93d8de2bf653689031ce8d62b0d
Matthew Jaurigue <matthewj321@berkeley.edu>
2026-08-19 14:03:48 -0700

    added doc strings

--- newton.py
+++ newton.py
@@ -1,9 +1,11 @@
 def derivative(fun):
+    """returns derivative of function"""
     eps = 0.00001
     return lambda x: (fun(x+eps) - fun(x)) / eps
 
     
 def optimize(start, fun):
+    """returns value of Newton's method"""
     xt = start
     xt2 = xt-derivative(fun)(xt)/derivative(derivative(fun))(xt)
     while abs(xt2 - xt) > 0.01:
